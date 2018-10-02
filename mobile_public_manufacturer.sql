5b96685f1f1de59a4a415c8e217756e37590580a
MVBiryukov-teacher-stc <m.biryukov@innopolis.ru>
2018-10-02 10:56:54 +0300

    Added SQL dumps

---
+++ mobile_public_manufacturer.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.manufacturer (id, name, country) VALUES (1, 'Siemens', 'Germany');
+INSERT INTO public.manufacturer (id, name, country) VALUES (2, 'Nokia', 'Finland');
+INSERT INTO public.manufacturer (id, name, country) VALUES (3, 'Motorola', 'USA');
+INSERT INTO public.manufacturer (id, name, country) VALUES (4, 'Samsung', 'Korea');
+INSERT INTO public.manufacturer (id, name, country) VALUES (5, 'Apple', 'USA');
---
+++ mobile_public_mobile.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.mobile (id, model, price, manufacturer_id) VALUES (2, '3310', 8000, 2);
+INSERT INTO public.mobile (id, model, price, manufacturer_id) VALUES (1, 'c35', 5000, 1);
+INSERT INTO public.mobile (id, model, price, manufacturer_id) VALUES (3, 'C25', 5000, 1);
+INSERT INTO public.mobile (id, model, price, manufacturer_id) VALUES (4, 'C380', 5000, 3);
+INSERT INTO public.mobile (id, model, price, manufacturer_id) VALUES (7, 'Apple Iphone 1', 70000, 3);
